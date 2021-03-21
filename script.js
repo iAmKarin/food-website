f3b33048f07faf5e08bf34560f0166ede907512a
Karin <karin.van.brecht@gmail.com>
2021-03-21 20:12:13 +0100

    images en script

---
+++ script.js
@@ -0,0 +1,15 @@
+const icons = document.querySelectorAll(".heading__icons i");
+let counter = 1;
+
+setInterval(() => {
+  counter++;
+
+  const icon = document.querySelector("i.change");
+  icon.classList.remove("change");
+  if (counter > icons.length) {
+    icons[0].classList.add("change");
+    counter = 1;
+  } else {
+    icon.nextElementSibling.classList.add("change");
+  }
+}, 2000);
